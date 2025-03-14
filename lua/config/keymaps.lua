9740c89512f2fbd58ed086633a94ef3df0a1717d
Jakob Westhoff <jakob@westhoffswelt.de>
2025-03-14 15:23:43 +0100

    Implemented project.nvim fzf-lua integration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@ vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in
 vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end,
     {desc="Open Diagnostics in Float"}
 )
+
+vim.keymap.set("n", "<leader>fp", function() require('project-fzf').projects() end)
